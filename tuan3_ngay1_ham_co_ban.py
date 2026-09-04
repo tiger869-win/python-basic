6127188da0058aef56d43358e538ef2b36ee4c26
unknown <levandau1986st@gmail.com>
2026-09-04 20:27:06 +0700

    tuan 3 ngay 1 - ham co ban

---
+++ tuan3_ngay1_ham_co_ban.py
@@ -0,0 +1,16 @@
+def chao_hoi():
+    print("Xin chào!")
+    print("Chúc mừng bạn đến với python")
+# Gọi hàm
+chao_hoi()
+chao_hoi()
+def chao_ten(ten):
+    print("Xin chào,", ten)
+chao_ten("Lê Văn Đấu")
+chao_ten("python")
+def tinh_tong(a, b):
+    ket_qua = a + b
+    return ket_qua
+tong = tinh_tong(10, 20)
+print("Tổng =", tong)
+print("Tổng khác =", tinh_tong(5, 7))         
